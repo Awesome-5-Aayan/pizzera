ec4e140efc557f873076e578910470fb8a1d87a6
Awesome-5-Aayan <81710455+Awesome-5-Aayan@users.noreply.github.com>
2021-08-24 16:13:16 -0500

    Add files via upload

--- main.js
+++ main.js
@@ -1,20 +1,20 @@
-var images = ["https://i.postimg.cc/MGn9GJXw/family.jpg","https://i.postimg.cc/qqyYvVbq/grandpa.jpg", "https://i.postimg.cc/wjMnFtMX/father.jpg" , "https://i.postimg.cc/5ymDKL83/bro.jpg", "https://i.postimg.cc/JnL6wtrd/sister.jpg", "https://i.postimg.cc/bw5W5zSK/mother.jpg"];
-var names = ["Fmaily Book","Ranbir Singh", "Diljeet Singh", "Rocky Singh", "Alia Singh", "Soni Singh"];
-var i = 0;
-function update()
-{
-    i++;
-    var numbers_of_family_member_in_array = 5
-    if(i > numbers_of_family_member_in_array)
-      {
-          i = 0;
-      }
-    
-    //Debug the code to store list of images in updatedImage. Use images[i]
-    var updatedImage = [images];
-    //Debug the code to store list of names in updatedName. Use names[i]
-    var updatedName = [names] ;
- 
-    document.getElementById("family_member_image").src = updatedImage;
-    document.getElementById("family_member_name").innerHTML = updatedName;
+name_of_student_array = [];
+function submit() {
+    var name_1 = document.getElementById("student1").value;
+    var name_2 = document.getElementById("student2").value;
+    var name_3 = document.getElementById("student3").value;
+    var name_4 = document.getElementById("student4").value;
+    name_of_student_array.push(name_1);
+    name_of_student_array.push(name_2);
+    name_of_student_array.push(name_3);
+    name_of_student_array.push(name_4);
+    console.log(name_of_student_array);
+    document.getElementById("display_name").innerHTML = name_of_student_array;
+    document.getElementById("submit_button").style.display = "none";
+    document.getElementById("sort_btn").style.display= "inline-block";
+}
+function sorting(){
+    name_of_student_array.sort();
+    console.log(name_of_student_array);
+    document.getElementById("display_name").innerHTML = name_of_student_array;
 }
